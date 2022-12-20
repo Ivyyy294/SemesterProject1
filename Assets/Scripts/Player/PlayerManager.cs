e44504b1ec819f15c48f24e18477e08b20d54973
Ivyyy <48586445+Ivyyy294@users.noreply.github.com>
2022-12-20 14:48:55 +0100

    NEW MenueScene
    Added first draft of PlayerLobby

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -2,27 +2,85 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class PlayerConfiguration
+{
+	public PlayerInput Input { get; set;}
+	public int PlayerIndex {get; set;}
+	public int TeamIndex {get; set;}
+	public bool IsReady { get; set; }
+	public Vector3 SpawnPoint {get; set;}
+
+	public PlayerConfiguration (PlayerInput pi)
+	{
+		PlayerIndex = pi.playerIndex;
+		Input = pi;
+	}
+}
 
 public class PlayerManager : MonoBehaviour
 {
-	[SerializeField]
-	private Transform[] spawnPoints = new Transform[2];
-	//bool waitingForPlayers = true;
+	//Public Values
+	public static PlayerManager Me { get; private set;}
 
+	//Editor Values
+	[SerializeField] int MaxPlayers = 2;
+
+	//Private Values
+	private List <PlayerConfiguration> playerConfigs;
+
+	//Public Functions
 	public void OnPlayerJoined (PlayerInput playerInput)
 	{
-		int playerId = playerInput.playerIndex;
-		int teamId = playerId % 2;
+		if (!playerConfigs.Any (p => p.PlayerIndex == playerInput.playerIndex))
+		{
+			playerInput.transform.SetParent (transform);
+			playerConfigs.Add (new PlayerConfiguration (playerInput));
+		}
 
-		GameStatus.Me.AddPlayerToTeam ( (uint)playerId, teamId);
 
-		GameObject player = playerInput.gameObject;
-		SetLayerID (player, 6 + teamId);
+		//int playerId = playerInput.playerIndex;
+		//int teamId = playerId % 2;
 
-		player.transform.position = spawnPoints[teamId].position;
-		player.transform.SetParent (transform);
+		//GameStatus.Me.AddPlayerToTeam ( (uint)playerId, teamId);
 
+		//GameObject player = playerInput.gameObject;
+		//player.transform.SetParent (transform);
+		//player.transform.position = spawnPoints[teamId].position;
+		//player.SetActive (false);
 		//waitingForPlayers = false;
+	}
+
+	public void SetPlayerTeam (int index, int team)
+	{
+		PlayerConfiguration c = playerConfigs[index];
+		c.TeamIndex = team;
+		SetLayerID (c.Input.gameObject, 6 + team);
+	}
+
+	public void ReadyPlayer (int index)
+	{
+		playerConfigs[index].IsReady = true;
+
+		if (playerConfigs.Count == MaxPlayers && playerConfigs.All(p => p.IsReady == true))
+		{
+			SceneManager.LoadScene ("Town Test");
+		}
+	}
+
+	//Private Functions
+	private void Awake()
+	{
+		if (Me != null)
+			Debug.Log ("Tyring o create another Instance of SIngelton!");
+		else
+		{
+			Me = this;
+			DontDestroyOnLoad (Me);
+				playerConfigs = new List<PlayerConfiguration>();
+		}
 	}
 
 	private void SetLayerID (GameObject obj, int layerId)
@@ -34,15 +92,4 @@ public class PlayerManager : MonoBehaviour
 			t.gameObject.layer = layerId;
 		}
 	}
-
-	//private void OnGUI()
-	//{
-	//	if (waitingForPlayers)
-	//	{
-	//		GUILayout.BeginArea(new Rect (Screen.width / 2, Screen.height / 2, 500, 500));
-	//		GUILayout.Label ("Waiting for players!");
-
-	//		GUILayout.EndArea();
-	//	}
-	//}
 }
